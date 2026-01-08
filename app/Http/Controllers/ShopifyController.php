fb259732d2edd45a8be54b79a34b9d446bb0c3f0
Olakiitan <ekundayoopeyemi123@gmail.com>
2026-01-09 00:16:15 +0100

    Render products index view in ShopifyController

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -9,11 +9,13 @@ use Illuminate\Support\Facades\Log;
 
 class ShopifyController extends Controller
 {
-    public function fetchProducts(ShopifyService $shopify)
+    public function index(ShopifyService $shopify)
     {
         try {
             $products = $shopify->getProducts(15);
-            dd($products);
+
+            return view('products.index', ['products' => $products]);
+            // dd($products);
         } catch (\Throwable $e) {
 
             Log::error("Shopify error: " . $e->getMessage());
