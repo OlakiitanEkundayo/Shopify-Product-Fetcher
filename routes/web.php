aa56258e6ec566b8c346b8624ee4cee4b8cee2f7
Olakiitan <ekundayoopeyemi123@gmail.com>
2026-01-08 23:50:06 +0100

    Add index route for ShopifyController

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,5 @@
 use App\Http\Controllers\ShopifyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/shopify-products', [ShopifyController::class, 'fetchProducts']);
+Route::get('/', [ShopifyController::class, 'index']);
