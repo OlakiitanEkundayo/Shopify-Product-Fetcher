cd9dea88f13804e5d71091c06c5f4b2fb94c7628
Olakiitan <ekundayoopeyemi123@gmail.com>
2026-01-14 11:02:36 +0100

    feat: add Product model for Shopify products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'shopify_id',
+        'title',
+        'vendor',
+        'product_type',
+        'stock_quantity',
+        'image_url',
+        'status'
+    ];
 }
