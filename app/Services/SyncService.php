a47f33eb61efbbfb3328def462b75b4e12b0871b
Olakiitan <ekundayoopeyemi123@gmail.com>
2026-01-16 17:11:37 +0100

    chore: create SyncService for handling Shopify sync logic

---
+++ app/Services/SyncService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+class SyncService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct() {}
+
+    public function syncProducts() {}
+}
